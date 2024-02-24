2b9929195d6a6e5806738eb56f2a00b6990bacb9
mehrdadkianianbohi <mehrdad.kian71@gmail.com>
2024-02-24 10:35:46 +0330

    add ORM prisma nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabseModule } from './databse/databse.module';
+import { DatabaseService } from './database/database.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabseModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
